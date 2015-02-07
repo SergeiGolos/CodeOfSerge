b3b308fdcfa5eae1d717facb20b13f3d8575a459
Sergei Golos <sergei.golos@gmail.com>
2015-02-07 12:45:39 -0500

    Sample unit test resolvers nancy app created.
    THis has a basic instance of rounting example.

---
+++ angular-unit-tesing-controller-resolvers/Content/app/app.js
@@ -0,0 +1,26 @@
+﻿(function(angular) {
+    'use strict';
+
+    var app = angular.module('app', ['ngRoute']).config([
+        '$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when('/Home/:message?', {
+                templateUrl: 'Home.html',
+                controller: 'home',
+                resolve: {
+                    data: ['$route', function ($route) {
+                        return "Test String Data" + $route.current.params.message;
+                    }]
+                }
+            }).otherwise({redirectTo: '/Home'});
+        }
+    ]);
+
+    app.controller('home', [
+        "$scope",
+        "data",
+        function($scope, data) {
+            $scope.message = data;
+        }
+    ]);
+})(window.angular);
---
+++ angular-unit-tesing-controller-resolvers/Modules/IndexModule.cs
@@ -0,0 +1,13 @@
+﻿namespace angular_unit_tesing_controller_resolvers.Modules
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["index"];
+            Get["/Data"] = _ => Response.AsJson(new { Value = "Test" });
+        }
+    }
+}

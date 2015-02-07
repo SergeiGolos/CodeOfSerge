69bc88642c94613cc27acc9cdc5834886f714158
Sergei Golos <sergei.golos@gmail.com>
2015-02-07 14:42:17 -0500

    Created Unit Tests
    Created instances of the unit tests for a resolver function.

--- angular-unit-tesing-controller-resolvers/Content/app/app.js
+++ angular-unit-tesing-controller-resolvers/Content/app/app.js
@@ -1,25 +1,47 @@
-﻿(function(angular) {
+﻿(function (angular) {
     'use strict';
 
-    var app = angular.module('app', ['ngRoute']).config([
+    var app = angular.module('app', ['ngRoute', 'ngLocale']).config([
         '$routeProvider',
-        function($routeProvider) {
+        'dataProvider',
+        function ($routeProvider, dataProvider) {
+            var data = dataProvider.$get();
             $routeProvider.when('/Home/:message?', {
                 templateUrl: 'Home.html',
                 controller: 'home',
                 resolve: {
-                    data: ['$route', function ($route) {
-                        return "Test String Data" + $route.current.params.message;
-                    }]
+                    data: data.home
                 }
-            }).otherwise({redirectTo: '/Home'});
+            }).otherwise({ redirectTo: '/Home' });
         }
     ]);
+
+    app.provider('data', function () {
+        return {
+            $get: function () {
+                return {
+                    home: [
+                        '$route',
+                        '$http',
+                        '$q',
+                        function ($route, $http, $q) {
+                            var deffer = $q.defer();
+                            $http.get('/Data').then(function (result) {
+                                deffer.resolve([result.data.value, $route.current.params.message].join(' '));
+                            });
+
+                            return deffer.promise;
+                        }
+                    ]
+                }
+            }
+        }
+    });
 
     app.controller('home', [
         "$scope",
         "data",
-        function($scope, data) {
+        function ($scope, data) {
             $scope.message = data;
         }
     ]);
---
+++ angular-unit-tesing-controller-resolvers/Content/test/app.test.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="../scripts/angular.js" />
+/// <reference path="../scripts/angular-route.js" />
+/// <reference path="../scripts/angular-mocks.js" />
+/// <reference path="../scripts/i18n/angular-locale_en-us.js" />
+/// <reference path="../app/app.js" />
+describe('Testing A Resolver dataProvider', function () {
+    var _data;
+    var _q;
+    var fn;
+    var _timeout;
+    beforeEach(function() {
+        angular.mock.module('app');
+        angular.mock.inject(['data', '$q', '$timeout', function(data, $q, $timeout) {
+            _timeout = $timeout;
+            _data = data;
+            _q = $q;
+            fn = _data.home[_data.home.length - 1];
+        }]);
+    });
+
+    it('The home resolve object is an array', function() {
+        expect(angular.isArray(_data.home)).toBe(true);
+    });
+
+    it('The last item on the object is a function', function () {        
+        expect(angular.isFunction(fn)).toBe(true);
+    });
+
+    describe('Execute the resolver', function() {
+        var _http;
+        var _route;
+
+        // Creating some mock objects to resolve inside my function.
+        beforeEach(function() {
+            _route = { current: { params: { message: 'test' } } }
+            _http = {
+                get: function() {
+                    var defer = _q.defer();
+                    defer.resolve({ data: { value: 'test' } });
+                    return defer.promise;
+                }
+            }
+        });
+
+        it('The the promise resolves to a combination of the get results and route messages', function() {
+            var result = fn(_route, _http, _q);
+
+            expect(angular.isFunction(result.then)).toBe(true);
+            result.then(function(data) {                
+                expect(data).toBe('test test');                
+            });
+
+            _timeout.flush();
+        });
+    });
+});

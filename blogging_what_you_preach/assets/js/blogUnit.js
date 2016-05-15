b4dd09390b19070ddfa9d2067097984264aa22be
Sergei Golos <sergei.golos@gmail.com>
2016-05-15 10:58:50 -0400

    a slight re-write of the unit test loading logic
    The updated logic give me much more control over the execution and
    loading of js.. It also give me the option to inject blanket into this
    for aditional cool code coverage feature in the future.

---
+++ blogging_what_you_preach/assets/js/blogUnit.js
@@ -0,0 +1,138 @@
+(function(window) {
+    var blogUnit = {};
+    //  We start by building a collection of code segments;
+    var blocks = {};    
+    function CodeBlock(name, source, language) {
+        this.name = name;
+        this.source = source;
+        this.language = language;
+        this.code = source.text();
+    }
+    
+    $(".language-blogunit").hide();
+    
+    CodeBlock.prototype.wrap = function(wrapperFn) {
+        return wrapperFn(this.name, this.code);
+    }
+    CodeBlock.prototype.isRunner = function() {
+        return this.language == "blogunit";
+    }
+    
+    CodeBlock.prototype.ready = function() {
+        var dependencies = _(["assets/js/boot.js"]).concat(this.code.split(/\n/g))
+            .map(_.trim)
+            .filter(function(line) {
+                return (line.indexOf("#") != 0) && line;
+            })
+            .map(function(item) {
+                return new Promise(function(resolve, reject){ 
+                   if (blocks[item]) {
+                       resolve(blocks[item].code);
+                       return;
+                   } 
+                   
+                   var xhr;
+                   if (window.XMLHttpRequest) {
+                        xhr = new XMLHttpRequest();
+                   } else if (window.ActiveXObject) {
+                        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+
+                    xhr.onreadystatechange = function(){ 
+                        if (xhr.readyState === 4) 
+                            resolve(xhr.responseText); 
+                    };
+                    xhr.open("GET", _.trim(item));
+                    xhr.send();                   
+                });                                                         
+            })
+            .value();                       
+        var frame = this.source.next()[0];   
+        Promise.all(dependencies).then(function(data) {
+            _.each(data, function(script) {             
+                frame.contentWindow.postMessage(script, "*");
+            })
+            frame.contentWindow.postMessage("window.blogUnitStart()", "*");
+        });
+    }
+    
+    CodeBlock.prototype.complete = function(message) {
+        var s = this.source;
+        s.next().height(message);                
+    }    
+
+    function createHarness(block) {
+        var content = encodeURI('<!DOCTYPE html>' + 
+            '<html><meta charset="utf-8">' + 
+            '<head>'+ 
+                '<title>Jasmine Spec Runner v2.4.1</title>' + 
+                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jasmine/2.4.1/jasmine.css">' +                 
+                '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jasmine/2.4.1/jasmine.js"></script>' + 
+                '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jasmine/2.4.1/jasmine-html.js"></script>' +                        
+                '<script type="text/javascript">' + 
+                    'window.runnerName = "' + block.name + '";' + 
+                    'window.parent.postMessage({ id: window.runnerName, state : "ready" }, "*");' +
+                    'window.addEventListener("message", function(event){' + 
+                        'var script = document.createElement("script");' + 
+                        'script.setAttribute("type", "text/javascript");' + 
+                        'script.text = event.data; ' + 
+                        'document.body.appendChild(script);' + 
+                    '});' +  
+                '</script>' +     
+            '</head>' + 
+            '<body></body>' + 
+        '</html>');
+        
+        var frame =  $("<iframe>");
+        frame.attr("src","data:text/html;charset=utf-8," +  content);
+        frame.attr("id", block.name);
+        frame.css({
+            border : "0px",
+            width : "100%",
+            margin : "0px",
+            padding: "0px"
+        });                          
+        return frame;                    
+    }
+    
+    blogUnit.initialize = function ($codeBlocks) {                  
+        var runners = [];
+        $codeBlocks.each(function(index, block) {                                               
+            var $block = $(block);
+            var nameObject = _($block.attr("class").split(" "))
+                .filter(function(item) { return item.indexOf("language-", "") == -1 || item.indexOf("name-") == -1; })            
+                .reduce(function(hash, value) {
+                    if (value) {
+                        var data = value.split("-");                    
+                        hash[data[0]] = data[1];
+                    }
+                    return hash;
+                    }, {});                                       
+            
+            blocks[nameObject.name] = new CodeBlock(nameObject.name, $block, nameObject.language);
+            if (blocks[nameObject.name].isRunner()) {
+                runners.push(blocks[nameObject.name]);
+            }            
+        });
+        window.addEventListener("message", function(event){
+            var fn = ((blocks[event.data.id] || {})[event.data.state] || function() { console.log("Function: " + event.data.state +" could not be found.")})                       
+            fn.apply(blocks[event.data.id], [event.data.message]);                        
+        });
+        
+        return runners;
+    }                               
+
+    blogUnit.run = function(runner) {         
+        var frame = createHarness(runner);        
+        frame.insertAfter(runner.source);                                       
+    }
+        
+    window.blogUnit = blogUnit;
+}(window));
+
+$(function() {    
+    var runners = blogUnit.initialize($("code"));
+    _.each(runners, function(runner) {
+        blogUnit.run(runner);
+    });                
+});
--- blogging_what_you_preach/assets/js/boot.js
+++ blogging_what_you_preach/assets/js/boot.js
@@ -1,49 +1,21 @@
-/**
- Starting with version 2.0, this file "boots" Jasmine, performing all of the necessary initialization before executing the loaded environment and all of a project's specs. This file should be loaded after `jasmine.js` and `jasmine_html.js`, but before any project source files or spec files are loaded. Thus this file can also be used to customize Jasmine for a project.
-
- If a project is using Jasmine via the standalone distribution, this file can be customized directly. If a project is using Jasmine via the [Ruby gem][jasmine-gem], this file can be copied into the support directory via `jasmine copy_boot_js`. Other environments (e.g., Python) will have different mechanisms.
-
- The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
-
- [jasmine-gem]: http://github.com/pivotal/jasmine-gem
- */
-
 (function() {
 
-  /**
-   * ## Require &amp; Instantiate
-   *
-   * Require Jasmine's core files. Specifically, this requires and attaches all of Jasmine's code to the `jasmine` reference.
-   */
   window.jasmine = jasmineRequire.core(jasmineRequire);
-
-  /**
-   * Since this is being run in a browser and the results should populate to an HTML page, require the HTML-specific Jasmine code, injecting the same reference.
-   */
   jasmineRequire.html(jasmine);
-
-  /**
-   * Create the Jasmine environment. This is used to run all specs in a project.
-   */
+  
   var env = jasmine.getEnv();
-
-  /**
-   * ## The Global Interface
-   *
-   * Build up the functions that will be exposed as the Jasmine public interface. A project can customize, rename or alias any of these functions as desired, provided the implementation remains unchanged.
-   */
   var jasmineInterface = jasmineRequire.interface(jasmine, env);
 
-  /**
-   * Add all of the Jasmine global/public interface to the global scope, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
-   */
   extend(window, jasmineInterface);
-
+function extend(destination, source) {
+    for (var property in source) destination[property] = source[property];
+    return destination;
+  }
   /**
    * ## Runner Parameters
    *
    * More browser specific code - wrap the query string in an object and to allow for getting/setting parameters from the runner user interface.
-   */
+   */  
 
   var queryString = new jasmine.QueryString({
     getWindowLocation: function() { return window.location; }
@@ -84,7 +56,6 @@
    */
   env.addReporter(jasmineInterface.jsApiReporter);
   env.addReporter(htmlReporter);
-
   /**
    * Filter which specs will be run by matching the start of the full name against the `spec` query param.
    */
@@ -111,12 +82,17 @@
    */
   var currentWindowOnload = window.onload;
 
-  window.onload = function() {
+  window.blogUnitStart = function() {
     if (currentWindowOnload) {
       currentWindowOnload();
     }
     htmlReporter.initialize();
-    env.execute();
+    env.execute();    
+    var body = document.body;
+    var html = document.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    window.parent.postMessage({ id: window.runnerName, state : "complete", message : height }, "*");
+    html.style.overflow="hidden";
   };
 
   /**
